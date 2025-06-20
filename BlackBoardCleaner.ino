d221a30845dda0b4afc3cf631d8e0368843fe4ab
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2025-06-20 20:48:12 +0900

    KICParser passed mini test

--- BlackBoardCleaner.ino
+++ BlackBoardCleaner.ino
@@ -164,36 +164,56 @@ ScheduleGateway* gateway;
 
 class KicParser {
 public:
-  static SPool* convertToSPool(String *kic) {
+  static SPool* convertToSPool(String& kic) {
     SPool* pool = new SPool(nullptr, 0);
     Serial.print("convertToSPool() called with: ");
-    Serial.println(*kic);
+    Serial.println(kic);
 
     // check '/' at end of the string
-    if (!kic->endsWith("/")) {
+    if (!kic.endsWith("/")) {
       return nullptr;
     }
 
     // devide the string with ';' to extract kicHeader
     unsigned int begin = 0;
-    String kicHeader = kic->substring(begin, kic->indexOf(';', begin));
+    String kicHeader = kic.substring(begin, kic.indexOf(';', begin));
     // kic += begin; // move kic pointer to next head of the block
 
     // devide the string with ';' to extract timeToSyc
-    begin = kic->indexOf(';', begin+1) + 1;
-    unsigned int timeToSyc = kic->substring(begin, kic->indexOf(';', begin)).toInt(); // HACK: type diff(unsigned int vs long)
+    begin = kic.indexOf(';', begin+1) + 1;
+    unsigned int timeToSyc = kic.substring(begin, kic.indexOf(';', begin)).toInt(); // HACK: type diff(unsigned int vs long)
     // kic += begin; // move kic pointer to next head of the block
 
     // start loop to extract each schedule with ';'
     unsigned int newCount = 0;
     Schedule *newSchedules = nullptr;
+    // Serial.print("convertToSPool> header: ");
+    // Serial.println(kicHeader);
+    // Serial.print("convertToSPool> timeHeader: ");
+    // Serial.println(timeToSyc);
+    // Serial.println("convertToSPool> Schedules@");
     while (true) {
-      if (!kic->compareTo(kic->substring(begin))) { // 0: if String equals myString2
+      if (!kic.substring(begin).compareTo("/")) { // 0: if String equals myString2
+        // Serial.println("@");
         break;
       }
-      begin = kic->indexOf(';', begin+1) + 1;
-      String scheduleBlock=kic->substring(begin, kic->indexOf(';', begin));
-      Schedule *schedule=convertToSchedule(&scheduleBlock);
+      begin = kic.indexOf(';', begin+1) + 1;
+      String scheduleBlock=kic.substring(begin, kic.indexOf(';', begin));
+      // Serial.print(">> ");
+      // Serial.println(scheduleBlock);
+      Schedule *schedule=convertToSchedule(scheduleBlock);
+      Serial.println("convertToSPool> converted(");
+      Serial.print("cmd=");
+      Serial.println(schedule->cmd);
+      Serial.println("hours@");
+      for(unsigned int i = 0; i < schedule->count; i++){
+        Serial.println(schedule->hours[i]);
+      }
+      Serial.println("@");
+      Serial.print("count=");
+      Serial.println(schedule->count);
+      Serial.println(")");
+
       queueSchedule(schedule, pool);
     }
 
@@ -201,19 +221,23 @@ public:
   }
 
 private:
-  static Schedule* convertToSchedule(const String *block) {
-    char cmd = block->charAt(0);
-    ++block;
+  static Schedule* convertToSchedule(const String& block) {
+    Serial.print("convertToSchedule called with: ");
+    Serial.println(block);
+    char cmd = block.charAt(0);
     Schedule* schedule=new Schedule(cmd, nullptr, 0);
 
-    unsigned int *newHours = nullptr;
-    unsigned int newCount = 0;
-    for (int i = 0; i + 3 < block->length(); i += 4) {
-      String chunk = block->substring(i, i + 3);
+    for (int i = 1; i + 4 <= block.length() + 1; i += 4) {
+      String chunk = block.substring(i, i + 4);
+      Serial.print("convertToSchedule> queueHours call with ");
+      Serial.println(chunk);
+      
       int hour = atoi(chunk.c_str());
       queueHours(hour, schedule);
     }
 
+    // Serial.print("convertToSchedule> cmd=");
+    // Serial.println(schedule->cmd);
     return schedule;
   }
 
@@ -235,50 +259,7 @@ private:
   KicParser() = delete;
 };
 
-/*
-typedef struct {
-  POINT **points;
-  unsigned int count;
-} PPOOL;
-
-static void freePool(PPOOL *const pool);
-static int pushPoint(const POINT *point, PPOOL *pool);
-static int isConcluded(const POINT *point, const PPOOL *pool);
-
-static void freePool(PPOOL *const pool) {
-  free(pool->points);
-  pool->points = NULL;
-  pool->count = 0;
-}
-
-static int pushPoint(const POINT *point, PPOOL *pool) {
-  POINT const **new_pool =
-      realloc(pool->points, (pool->count + 1) * sizeof(POINT *));
-  if (!new_pool) {
-    return 0;
-  }
-
-  *(new_pool + pool->count) = point;
-  pool->points = (POINT **)new_pool;
-  pool->count++;
-
-  return 1;
-}
-
-static int isConcluded(const POINT *point, const PPOOL *pool) {
-  for (const POINT *const *it = (const POINT *const *)pool->points;
-       it != (const POINT *const *)pool->points + pool->count; ++it) {
-    if (*it == point) {
-      return 1;
-    }
-  }
-  return 0;
-}
-*/
-
-/*
-String.end(String str) => '\0'
-*/
+static SPool* spool;
 
 void setup() {
   Serial.begin(9600);
@@ -290,6 +271,50 @@ void loop() {
   clearSerialBuffer();
   if (gateway->available() <= 0) return;
   String str = gateway->receiveString();
+  Serial.print("receivedString: ");
+  Serial.println(str);
 
-  KicParser::convertToSPool(&str);
+  spool = KicParser::convertToSPool(str);
+  Serial.println("=== TEST ===");
+  for(unsigned int i = 0; i < spool->count; i++){
+    Schedule *schedule = *(spool->schedules + i);
+    Serial.print("Schedule ");
+    Serial.println(i);
+    Serial.print("cmd> ");
+    Serial.println(schedule->cmd);
+    Serial.println("hours@");
+    for(unsigned int j = 0; j < schedule->count; j++){
+      Serial.println(schedule->hours[j]);
+    }
+    Serial.println("@");
+  }
+  Serial.println("=== TEST END ===");
+
+// === TEST ===
+// Schedule 0
+// cmd> 1
+// hours@
+// 900
+// 1000
+// 1130
+// @
+// Schedule 1
+// cmd> 2
+// hours@
+// 800
+// 900
+// @
+// Schedule 2
+// cmd> 8
+// hours@
+// @
+// Schedule 3
+// cmd> 9
+// hours@
+// @
+// Schedule 4
+// cmd> /
+// hours@
+// @
+// === TEST END ===
 }
