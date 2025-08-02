a05d3faa6066c7ff9ee37d195c8b1bd00af1de28
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2025-08-02 21:08:44 +0900

    feat(WiFiConnector, WiFiConnectorTestCase, WirelessGatewayTestCase ,WiFiConnectionTestCase)!: delete WirelessGatewayTestCase, WiFiConnectionTestCase, add WiFiConnector, TestCase for one to substitute it for WirelessGateway

---
+++ lib/WiFiConnector.hpp
@@ -0,0 +1,9 @@
+#ifndef _WiFiCONNECTOR_
+#define _WiFiCONNECTOR_
+
+namespace WiFiConnector {
+    void setup();
+    bool available();
+}
+
+#endif // !_WiFiCONNECTOR_
---
+++ test/WiFiConnectorTestCase.hpp
@@ -0,0 +1,10 @@
+#ifndef _WiFiCONNECTORTESTCASE_
+#define _WiFiCONNECTORTESTCASE_
+
+namespace WiFiConnectorTestCase {
+    bool testConnect();
+    bool testAvailable();
+    void runAllTests();
+}
+
+#endif // !_WiFiCONNECTORTESTCASE_
