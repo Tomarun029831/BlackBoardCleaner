28f4c05391ee122e65f4d67059b82a9be8326678
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2025-08-10 13:31:02 +0900

    lang(ALL)!: replace all files to add rust-branch

--- lib/WiFiConnector.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef _WiFiCONNECTOR_
-#define _WiFiCONNECTOR_
-
-namespace WiFiConnector {
-    void setup();
-    bool available();
-}
-
-#endif // !_WiFiCONNECTOR_
--- test/WheelControllerManualTestCase.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef _WHEELCONTROLLERMANUALTESTCASE_
-#define _WHEELCONTROLLERMANUALTESTCASE_
-
-namespace WheelControllerManualTestCase {
-    void runAllTests();
-}
-
-#endif // !_WHEELCONTROLLERMANUALTESTCASE_
