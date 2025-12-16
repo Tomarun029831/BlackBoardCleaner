1489b36d4ea09eaa2770c675ec9c81ac714929c0
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2025-12-16 16:12:58 +0900

    chore(config_format): revise code

--- config_format.hpp
+++ config_format.hpp
@@ -1,5 +1,8 @@
+#define ID "<id>"
+#define WIPYSERVERURL "https://<domain>/<path>?id=" ID
+
 namespace CONFIG {
 const char *const SSID = "";
 const char *const PASSWORD = "";
-const char *const APIENDPOINT = "";
+const char *const APIENDPOINT = WIPYSERVERURL;
 } // namespace CONFIG
