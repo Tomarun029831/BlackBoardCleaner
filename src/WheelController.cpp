c10c5a7e841c76458563303347c88761e9ffaedb
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2026-02-16 20:54:50 +0900

    fix(WheelController): fix each number of pins

--- src/WheelController.cpp
+++ src/WheelController.cpp
@@ -12,10 +12,10 @@ namespace WheelController {
 // =====================
 // Motor pin definitions
 // =====================
-static constexpr gpio_num_t LEFT_MOTOR_PIN0 = (gpio_num_t)32;
-static constexpr gpio_num_t LEFT_MOTOR_PIN1 = (gpio_num_t)33;
-static constexpr gpio_num_t RIGHT_MOTOR_PIN0 = (gpio_num_t)25;
-static constexpr gpio_num_t RIGHT_MOTOR_PIN1 = (gpio_num_t)26;
+static constexpr gpio_num_t LEFT_MOTOR_PIN0 = (gpio_num_t)33;
+static constexpr gpio_num_t LEFT_MOTOR_PIN1 = (gpio_num_t)32;
+static constexpr gpio_num_t RIGHT_MOTOR_PIN0 = (gpio_num_t)26;
+static constexpr gpio_num_t RIGHT_MOTOR_PIN1 = (gpio_num_t)25;
 
 // =====================
 // Timing constants
